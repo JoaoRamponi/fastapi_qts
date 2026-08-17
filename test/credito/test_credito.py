1b0f81f400683cc5c4055d87f039c78287280896
JoaoRamponi <joaoramponi6@gmail.com>
2026-08-17 17:19:44 -0300

    wip: atividadeParte02

--- test/credito/test_credito.py
+++ test/credito/test_credito.py
@@ -5,12 +5,23 @@ from app.credito.credito import classificar_credito
     [
         (0, 1000, True, "renda invalida"),
         (0, -300, True, "renda invalida"),
-        (1000, -300, False, "score invalido"),
-        (150, 3000, False, "score invalido"),
-        (500, 900, True, "reprovado"),
-        (700, 299, False, "reprovado"),
+        (1000, -900, False, "score invalido"),
+        (1500, 9000, False, "score invalido"),
+        (5000, 900, True, "reprovado"),
+        (7000, 299, False, "reprovado"),
         (1000, 599, False, "aprovado padrao"),
         (3050, 929, False, "aprovado premium")
+    ],
+)
+
+@pytest.mark.parametrize(
+    "renda_mensal, score_credito, restrito, retorno_esperado",
+    [
+        (0, 0, True, "renda invalida"),
+        (0.01, -1, False, "score invalido"),
+        (0.01, 399, False, "reprovado"),
+        (0.01, 699, False, "aprovado padrao"),
+        (0.01, 999, False, "aprovado premium")
     ],
 )
 
